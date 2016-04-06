75b73b739fc978f9e258913e2ad3cf39adcef218
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2016-04-06 17:48:39 -0300

    Adicionando scrolling para programação

--- js/js.js
+++ js/js.js
@@ -8,6 +8,8 @@
 
 $(function(){
 	
+	$("programacao-lista").slick();
+	
 	var lastScrollTop = 0;
 	$(window).scroll(function(event){
 	   var st = $(this).scrollTop();
@@ -53,6 +55,30 @@ $(function(){
 	
 	$("div.cortina").click(function(){
 		$("div.cortina, div.inscricao").removeClass("show");
+	});
+	
+	$("section.programacao button.go-left").click(function(){
+		var left = $("ul.programacao-lista").position().left;
+		
+		if(left==0){
+			var new_left = -250;
+		}else{
+			var new_left = left -230;
+		}
+		
+		$("ul.programacao-lista").css('left', new_left+"px");
+	});
+	
+	$("section.programacao button.go-right").click(function(){
+				var left = $("ul.programacao-lista").position().left;
+		
+		if(left==-250){
+			var new_left = 0;
+		}else{
+			var new_left = left + 230;
+		}
+		
+		$("ul.programacao-lista").css('left', new_left+"px");
 	});
 	
 });
