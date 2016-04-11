57e75f8f02e7a26dca47a4ee9dc3205f6085227b
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2016-04-11 02:16:57 -0300

    adicionando seção inscrição

--- js/js.js
+++ js/js.js
@@ -9,6 +9,18 @@
 $(function(){
 	
 	//$("programacao-lista").slick();
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+			$('html, body').animate({
+			  scrollTop: target.offset().top - 59
+			}, 1000);
+			return false;
+		  }
+		}
+	  });
 	
 	function crop(){
 		var width = $("section.programacao").width() - 40;
